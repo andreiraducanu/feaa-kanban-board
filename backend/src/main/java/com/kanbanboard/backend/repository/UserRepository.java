410cbbd9fcbf49799dfdd72ba63ffb1e7c9d9c75
Raducanu Andrei <raducanu.andrei98@gmail.com>
2020-11-08 00:30:41 +0200

    Backend start

---
+++ backend/src/main/java/com/kanbanboard/backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.kanbanboard.backend.repository;
+
+import com.kanbanboard.backend.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+}
