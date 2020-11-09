c0f5a28880d4de475fe1f90444f6543271b802da
BotezMihai <37815111+BotezMihai@users.noreply.github.com>
2020-11-09 23:40:12 +0200

    Register and login
    Spring security added
    Register modified to support BCryptPasswordEncoder
    Still working on login

--- backend/src/main/java/com/kanbanboard/backend/controller/UserController.java
+++ backend/src/main/java/com/kanbanboard/backend/controller/UserController.java
@@ -1,20 +1,17 @@
 package com.kanbanboard.backend.controller;
 
 import com.kanbanboard.backend.model.User;
-import com.kanbanboard.backend.repository.UserRepository;
 import com.kanbanboard.backend.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.DigestUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+
 
 @RestController
 @RequestMapping("/user")
@@ -23,17 +20,19 @@ public class UserController {
     Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private UserService userService;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody User user) throws UnsupportedEncodingException {
 
         logger.info("register called");
-        String hash = userService.getMD5("pass");
+//        String hash = userService.getMD5("pass");
 
         if (userService.checkIfUserExits(user.getUsername())) {
             logger.info("User exists");
@@ -41,9 +40,17 @@ public class UserController {
         }
 
         logger.info("User doesn't exists");
-        user.setPassword(userService.getMD5(user.getPassword()));
+//        user.setPassword(userService.getMD5(user.getPassword()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        logger.info(user.getPassword());
         userService.saveNewUser(user);
         return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+
+        return new ResponseEntity<String>("Hahah", HttpStatus.OK);
     }
 
 
--- backend/src/main/java/com/kanbanboard/backend/service/UserService.java
+++ backend/src/main/java/com/kanbanboard/backend/service/UserService.java
@@ -1,17 +1,22 @@
 package com.kanbanboard.backend.service;
 
+import com.kanbanboard.backend.UserPrincipal;
 import com.kanbanboard.backend.model.User;
 import com.kanbanboard.backend.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 import java.io.UnsupportedEncodingException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -39,4 +44,14 @@ public class UserService {
     }
 
 
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(userName);
+        if (user == null) {
+            logger.info("user doesn't exists");
+            throw new UsernameNotFoundException(userName);
+        }
+        logger.info("USER EXITS");
+        return new UserPrincipal(user);
+    }
 }
