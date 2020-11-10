9195105d17a4418a3d6920d51fb383f01b60d8bc
BotezMihai <37815111+BotezMihai@users.noreply.github.com>
2020-11-11 00:08:08 +0200

    Update JwtUtil.java

--- backend/src/main/java/com/kanbanboard/backend/util/JwtUtil.java
+++ backend/src/main/java/com/kanbanboard/backend/util/JwtUtil.java
@@ -3,6 +3,7 @@ package com.kanbanboard.backend.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,9 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
     private String SECRET_KEY = "secret";
+
+    @Value("${security.secret}")
+    private String SECRET_KEY;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
