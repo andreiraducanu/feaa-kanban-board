67d209897b57bf16c24a9fe13e29e81184cbc622
BotezMihai <37815111+BotezMihai@users.noreply.github.com>
2020-11-11 00:09:19 +0200

    Update UserService.java

--- backend/src/main/java/com/kanbanboard/backend/service/UserService.java
+++ backend/src/main/java/com/kanbanboard/backend/service/UserService.java
@@ -6,14 +6,10 @@ import com.kanbanboard.backend.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.util.DigestUtils;
-
-import java.io.UnsupportedEncodingException;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -26,13 +22,6 @@ public class UserService implements UserDetailsService {
     public UserService(UserRepository userRepository) {
 
         this.userRepository = userRepository;
-    }
-
-    public String getMD5(String password) throws UnsupportedEncodingException {
-        logger.info("getMD5 function");
-        byte[] message = password.getBytes("UTF-8");
-        String hash = DigestUtils.md5DigestAsHex(message);
-        return hash;
     }
 
     public Boolean checkIfUserExits(String userName) {
@@ -49,10 +38,8 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(userName);
         if (user == null) {
-            logger.info("user doesn't exists");
             throw new UsernameNotFoundException(userName);
         }
-        logger.info("USER EXISTS");
         return new UserPrincipal(user);
     }
 }
