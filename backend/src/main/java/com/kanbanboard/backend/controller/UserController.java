547f7d0e2e61a6f4cf0ba7595240ba7ee33ee78a
BotezMihai <37815111+BotezMihai@users.noreply.github.com>
2020-11-10 22:40:13 +0200

    merge

---
+++ backend/src/main/java/com/kanbanboard/backend/controller/UserController.java
@@ -0,0 +1,85 @@
+package com.kanbanboard.backend.controller;
+
+import com.kanbanboard.backend.model.AuthenticationRequest;
+import com.kanbanboard.backend.model.AuthenticationResponse;
+import com.kanbanboard.backend.model.User;
+import com.kanbanboard.backend.service.UserService;
+import com.kanbanboard.backend.util.JwtUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    private UserService userService;
+    private PasswordEncoder passwordEncoder;
+    private AuthenticationManager authenticationManager;
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    public UserController(UserService userService, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user) throws UnsupportedEncodingException {
+
+        logger.info("register called");
+//        String hash = userService.getMD5("pass");
+
+        if (userService.checkIfUserExits(user.getUsername())) {
+            logger.info("User exists");
+            return new ResponseEntity<>("Choose another username! It already exists!", HttpStatus.CONFLICT);
+        }
+
+        logger.info("User doesn't exists");
+//        user.setPassword(userService.getMD5(user.getPassword()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        logger.info(user.getPassword());
+        userService.saveNewUser(user);
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password");
+        }
+
+        final UserDetails userDetails = userService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        logger.info("JWT ESTE " +jwt);
+
+        return new ResponseEntity<>(new AuthenticationResponse(jwt), HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public String testRoute(@RequestBody String test){
+
+        return test;
+    }
+}
---
+++ backend/src/main/java/com/kanbanboard/backend/service/UserService.java
@@ -0,0 +1,58 @@
+package com.kanbanboard.backend.service;
+
+import com.kanbanboard.backend.UserPrincipal;
+import com.kanbanboard.backend.model.User;
+import com.kanbanboard.backend.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import java.io.UnsupportedEncodingException;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    public String getMD5(String password) throws UnsupportedEncodingException {
+        logger.info("getMD5 function");
+        byte[] message = password.getBytes("UTF-8");
+        String hash = DigestUtils.md5DigestAsHex(message);
+        return hash;
+    }
+
+    public Boolean checkIfUserExits(String userName) {
+        return userRepository.existsByUsername(userName);
+    }
+
+    public void saveNewUser(User user) {
+        logger.info("New user is saved");
+        userRepository.save(user);
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(userName);
+        if (user == null) {
+            logger.info("user doesn't exists");
+            throw new UsernameNotFoundException(userName);
+        }
+        logger.info("USER EXISTS");
+        return new UserPrincipal(user);
+    }
+}
