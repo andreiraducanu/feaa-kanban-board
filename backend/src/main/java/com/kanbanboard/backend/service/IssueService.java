054d7f8ef3fe07c2947b076a509611c2609f2792
BotezMihai <37815111+BotezMihai@users.noreply.github.com>
2020-11-12 23:50:21 +0200

    Update IssueService.java

--- backend/src/main/java/com/kanbanboard/backend/service/IssueService.java
+++ backend/src/main/java/com/kanbanboard/backend/service/IssueService.java
@@ -1,5 +1,6 @@
 package com.kanbanboard.backend.service;
 
+import com.kanbanboard.backend.dto.CommentDto;
 import com.kanbanboard.backend.dto.IssueDto;
 import com.kanbanboard.backend.dto.WorkLogCreationDto;
 
@@ -9,5 +10,14 @@ public interface IssueService {
 
     IssueDto addWorklog(String id, WorkLogCreationDto workLogCreationDto);
 
-    public void deleteWorklog(String idIssue, String idWorklog);
+    void deleteWorklog(String idIssue, String idWorklog);
+
+    IssueDto addComment(CommentDto commentDto, String id);
+
+    IssueDto changeComment(CommentDto commentDto, String idIssue, String idComment);
+
+    String deleteComment(String idIssue, String idComment);
+
+    IssueDto addChild(String id, IssueDto issueDto);
+
 }
