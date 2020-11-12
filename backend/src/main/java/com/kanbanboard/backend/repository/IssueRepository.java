c03bacf21f2f33a3047d5117f7a5c227994de30c
BotezMihai <37815111+BotezMihai@users.noreply.github.com>
2020-11-12 23:50:16 +0200

    Update IssueRepository.java

--- backend/src/main/java/com/kanbanboard/backend/repository/IssueRepository.java
+++ backend/src/main/java/com/kanbanboard/backend/repository/IssueRepository.java
@@ -1,7 +1,9 @@
 package com.kanbanboard.backend.repository;
 
+import com.kanbanboard.backend.model.Comment;
 import com.kanbanboard.backend.model.Issue;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface IssueRepository extends MongoRepository<Issue, String> {
+
 }
