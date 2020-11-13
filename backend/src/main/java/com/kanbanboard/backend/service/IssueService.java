5daf0f5efc3b94bdd701333791b59620eeaa5a44
BotezMihai <37815111+BotezMihai@users.noreply.github.com>
2020-11-13 16:17:46 +0200

    Update IssueService.java

--- backend/src/main/java/com/kanbanboard/backend/service/IssueService.java
+++ backend/src/main/java/com/kanbanboard/backend/service/IssueService.java
@@ -1,31 +1,33 @@
 package com.kanbanboard.backend.service;
 
 import com.kanbanboard.backend.dto.*;
+import com.kanbanboard.backend.exception.EntityNotFoundException;
+import com.kanbanboard.backend.exception.ServerException;
 import com.kanbanboard.backend.model.WorkLog;
 
 public interface IssueService {
 
     IssueDto create(IssueCreateDto issueCreateDto);
 
-    IssueDto getById(String idIssue);
+    IssueDto getById(String idIssue) throws EntityNotFoundException;
 
-    IssueDto updateById(String idIssue, IssueUpdateDto issueUpdateDto);
+    IssueDto updateById(String idIssue, IssueUpdateDto issueUpdateDto) throws EntityNotFoundException;
 
-    String deleteById(String idIssue);
+    String deleteById(String idIssue) throws EntityNotFoundException;
 
-    IssueDto addChild(String idIssue, IssueAddChildDto issueAddChildDto);
+    IssueDto addChild(String idIssue, IssueAddChildDto issueAddChildDto) throws EntityNotFoundException, ServerException;
 
-    IssueDto removeChild(String idIssue, IssueRemoveChildDto issueRemoveChildDto);
+    IssueDto removeChild(String idIssue, IssueRemoveChildDto issueRemoveChildDto) throws EntityNotFoundException, ServerException;
 
-    WorkLogDto addWorkLog(String idIssue, WorkLogCreateDto workLogCreateDto);
+    WorkLogDto addWorkLog(String idIssue, WorkLogCreateDto workLogCreateDto) throws EntityNotFoundException, ServerException;
 
-    WorkLogDto updateWorkLog(String idIssue, String idWorkLog, WorkLogUpdateDto workLogUpdateDto);
+    WorkLogDto updateWorkLog(String idIssue, String idWorkLog, WorkLogUpdateDto workLogUpdateDto) throws EntityNotFoundException, ServerException;
 
-    String deleteWorkLog(String idIssue, String idWorkLog);
+    String deleteWorkLog(String idIssue, String idWorkLog) throws EntityNotFoundException, ServerException;
 
-    CommentDto addComment(String idIssue, CommentCreateDto commentCreateDto);
+    CommentDto addComment(String idIssue, CommentCreateDto commentCreateDto) throws EntityNotFoundException, ServerException;
 
-    CommentDto updateComment(String idIssue, String idComment, CommentUpdateDto commentDto);
+    CommentDto updateComment(String idIssue, String idComment, CommentUpdateDto commentDto) throws EntityNotFoundException, ServerException;
 
-    String deleteComment(String idIssue, String idComment);
+    String deleteComment(String idIssue, String idComment) throws EntityNotFoundException, ServerException;
 }
