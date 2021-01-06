a83fc798f51d1170a84a857c4b304cb681b37afb
BotezMihai <37815111+BotezMihai@users.noreply.github.com>
2021-01-06 21:02:23 +0200

    Create UserRegisterResponseDto.java

---
+++ backend/src/main/java/com/kanbanboard/backend/dto/UserRegisterResponseDto.java
@@ -0,0 +1,13 @@
+package com.kanbanboard.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserRegisterResponseDto {
+
+    private String message;
+
+    private String status;
+}
