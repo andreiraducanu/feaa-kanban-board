80d27b1c5757700ae45bfb85130c4ae24212ab42
BotezMihai <37815111+BotezMihai@users.noreply.github.com>
2021-01-06 21:02:20 +0200

    Update JwtDto.java

--- backend/src/main/java/com/kanbanboard/backend/dto/JwtDto.java
+++ backend/src/main/java/com/kanbanboard/backend/dto/JwtDto.java
@@ -2,10 +2,17 @@ package com.kanbanboard.backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class JwtDto {
 
     private final String jwt;
+
+    private String message;
+
+    private String status;
+
 }
