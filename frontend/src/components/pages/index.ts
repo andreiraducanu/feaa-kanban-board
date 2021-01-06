e6d33327920b44f00f63b2694863726ee08653e1
alexadiana <37219230+alexadiana@users.noreply.github.com>
2021-01-06 13:37:55 +0200

    Removed files

--- frontend/src/components/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as LoginPage } from './LoginPage';
-export { default as SignUpPage } from './SignUpPage';
-export { default as DashboardPage } from './DashboardPage';
-export { default as HomePage } from './HomePage';
